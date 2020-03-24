c2637cae10a06f521de6e9bc1560699617684df3
Melanie Tran <melaniexx02@gmail.com>
2020-03-24 12:51:15 -0600

    Created add dish method and add dish test

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,7 +1,13 @@
 class Potluck
-  attr_reader :date
+  attr_reader :date,
+              :dishes
 
   def initialize(date)
     @date = date
+    @dishes = []
+  end
+
+  def add_dish(new_dish)
+    @dishes << (new_dish)
   end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -10,4 +10,26 @@ class PotluckTest < Minitest::Test
 
     assert_instance_of Potluck, potluck
   end
+
+  def test_it_has_attributes
+    potluck = Potluck.new("7-13-18")
+
+    assert_equal "7-13-18", potluck.date
+    assert_equal [], potluck.dishes
+  end
+
+  def test_it_can_add_new_dish
+    potluck = Potluck.new("7-13-18")
+    assert_equal [], potluck.dishes
+
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    potluck.add_dish(couscous_salad)
+    assert_equal [couscous_salad], potluck.dishes
+
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck.add_dish(cocktail_meatballs)
+    assert_equal [couscous_salad, cocktail_meatballs], potluck.dishes
+
+    assert_equal 2, potluck.dishes.length
+  end
 end
